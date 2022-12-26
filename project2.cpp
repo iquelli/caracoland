b3c3e63fc4f5191e639f0bfa2b764c882cd7122e
Raquel <raquelbb28.08@gmail.com>
2022-12-26 13:10:25 +0000

    added main file

---
+++ project2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int vertices;
+int arcs;
+
+//TODO finish
+
+int main() {
+    scanf("%d", &vertices);
+    scanf("%d", &arcs);
+
+    for(int i = 0; i < arcs; i++) {
+        scanf("%d %d %d");
+    }
+
+    return 0;
+}
